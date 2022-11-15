131e6edb8b0d15fb3623a054308594721da40a80
dzwg <paule22@protonmail.com>
2022-11-15 16:15:55 +0100

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,10 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import NavigationComponent from './components/navigation/NavigationComponent.vue'
 </script>
 
 <template>
+  <navigation-component />
   <router-view></router-view>
-
-  <!--  <div class="logo-box">-->
-  <!--    <img class="logo vite" src="./assets/vite.svg" >-->
-  <!--    <img class="logo electron" src="./assets/electron.svg" >-->
-  <!--    <img class="logo vue" src="./assets/vue.svg" >-->
-  <!--  </div>-->
-  <!--  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />-->
-  <!--  <div class="static-public">-->
-  <!--    Place static files into the <code>/public</code> folder-->
-  <!--    <img style="width:77px;" :src="'./node.png'" >-->
-  <!--  </div>-->
 </template>
